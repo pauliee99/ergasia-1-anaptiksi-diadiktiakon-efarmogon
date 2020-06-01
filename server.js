dd8be5b880081c7f3990bfa7d7543e6056afcd2f
Pavlos Andreou <pavlosan67@gmail.com>
2020-06-02 00:53:50 +0300

    commid insert

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -8,21 +9,28 @@ app.use(bodyParser.json());
 const books = [];
 
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('./server.sqlite3')
 
-db.each("SELECT * FROM books", function(err, row){
-    if (!err){
-        console.log(JSON.stringify(row));//ektipni
-        books.push(row);//vali ta sto books
+// open database in memory
+const dbPath = path.resolve(__dirname, './server.sqlite3')
+let db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      return console.error(err.message);
     }
+    console.log('Connected to the in-rmemoy SQlite database.');
 });
 app.get(function(req, res){
-    db.run(`INSERT INTO books(id, author, title, genre, price) VALUES(req.params.id, req.params.author, req.params.title, req.params.genre, req.params.price)`, ['C'], function(err) {
+    db.run('INSERT INTO books(author, title, genre, price) VALUES(req.params.author, req.params.title, req.params.genre, req.params.price)', ['C'], function(err) {
         if (err) {
           return console.log(err.message);
         }
         console.log(`A row has been inserted with rowid ${this.lastID}`);
     });
+});
+db.each("SELECT * FROM books", function(err, row){
+    if (!err){
+        console.log(JSON.stringify(row));//ektipni
+        books.push(row);//vali ta sto books
+    }
 });
 
 
@@ -46,5 +54,15 @@ app.get('/getBook/:title', function(req,res){
     }
     res.send(result);
 });
+
+/*
+// close the database connection
+db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
+*/
 
 app.listen(3000);
